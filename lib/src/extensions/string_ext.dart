f5f24784add0a91bd38c47d2fba94bcb26fcaf0a
Steve Gehrman <sgehrman@gmail.com>
2020-05-16 22:18:11 -0700

    fix

--- lib/src/extensions/string_ext.dart
+++ lib/src/extensions/string_ext.dart
@@ -10,7 +10,7 @@ extension StringUtils on String {
   String fromCamelCase() {
     String displayName = '';
     bool lastUpper = false;
-    for (final String r in chars) {
+    for (final String r in characters) {
       if (r.toUpperCase() == r) {
         displayName += lastUpper ? r : ' $r';
 

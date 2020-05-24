8dda5ea34e78a2d33b6cb712e70c5f6ecd2a2f40
Steve Gehrman <sgehrman@gmail.com>
2020-05-23 21:10:35 -0700

    fixes

--- lib/src/extensions/num_ext.dart
+++ lib/src/extensions/num_ext.dart
@@ -13,9 +13,14 @@ extension ExtendedInt on int {
       return '0 Bytes';
     }
     const k = 1024;
-    final dm = decimals <= 0 ? 0 : decimals;
     final sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     final i = (math.log(this) / math.log(k)).floor();
+
+    int dm = math.max(0, decimals);
+    if (i == 0) {
+      // don't want 12.00 Bytes
+      dm = 0;
+    }
 
     return '${(this / math.pow(k, i)).toStringAsFixed(dm)} ${sizes[i]}';
   }

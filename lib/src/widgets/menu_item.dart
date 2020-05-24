5d1b0756467f7fa132d502daad657188a7244c45
Steve Gehrman <sgehrman@gmail.com>
2020-05-24 14:55:57 -0700

    add menuitem

---
+++ lib/src/widgets/menu_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({this.name, this.icon});
+
+  final String name;
+  final Icon icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 12.0),
+          child: icon,
+        ),
+        Text(name),
+      ],
+    );
+  }
+}

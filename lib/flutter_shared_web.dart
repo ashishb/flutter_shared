e7ff566fc61be1a45529b177928997e935a2b54a
Steve Gehrman <sgehrman@gmail.com>
2020-06-13 05:38:31 -0700

    web lib

---
+++ lib/flutter_shared_web.dart
@@ -0,0 +1,54 @@
+library flutter_shared_web;
+
+export 'src/dialogs/alert_dialog.dart';
+export 'src/dialogs/confirm_dialog.dart';
+export 'src/dialogs/string_dialog.dart';
+export 'src/dialogs/widget_dialog.dart';
+
+export 'src/extensions/canvas_ext.dart';
+export 'src/extensions/color_ext.dart';
+export 'src/extensions/date_ext.dart';
+export 'src/extensions/duration_ext.dart';
+export 'src/extensions/map_ext.dart';
+export 'src/extensions/num_ext.dart';
+export 'src/extensions/size_ext.dart';
+export 'src/extensions/string_ext.dart';
+
+export 'src/hive_db/hive_box.dart';
+export 'src/hive_db/hive_data.dart';
+export 'src/hive_db/hive_opener.dart';
+export 'src/hive_db/hive_utils.dart';
+
+export 'src/themes/dynamic_theme.dart';
+export 'src/themes/editor/theme_set.dart';
+export 'src/themes/editor/theme_set_button.dart';
+export 'src/themes/editor/theme_set_manager.dart';
+export 'src/themes/themes.dart';
+
+export 'src/utils/debouncer.dart';
+export 'src/utils/directory_listing_spec.dart';
+export 'src/utils/preferences.dart';
+export 'src/utils/server_file.dart';
+export 'src/utils/stack.dart';
+export 'src/utils/string_utils.dart';
+export 'src/utils/utils.dart';
+
+export 'src/widgets/bottom_navbar/bottom_navbar.dart';
+export 'src/widgets/bottom_slideup_sheet.dart';
+export 'src/widgets/browser_header.dart';
+export 'src/widgets/checkerboard_container.dart';
+export 'src/widgets/colored_button.dart';
+export 'src/widgets/dialog_button.dart';
+export 'src/widgets/draggable_scrollbar.dart';
+export 'src/widgets/file_info.dart';
+export 'src/widgets/image/image_viewer.dart';
+export 'src/widgets/image/super_image.dart';
+export 'src/widgets/json_widget.dart';
+export 'src/widgets/list_row.dart';
+export 'src/widgets/loading_widget.dart';
+export 'src/widgets/menu_item.dart';
+export 'src/widgets/preview_dialog.dart';
+export 'src/widgets/svg_asset.dart';
+export 'src/widgets/svg_image.dart';
+export 'src/widgets/theme_button.dart';
+export 'src/widgets/video_player_widget.dart';

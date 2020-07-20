f790458c6c1b2bea254f1f65967ddb75586c0f95
Steve Gehrman <sgehrman@gmail.com>
2020-07-20 05:00:01 -0700

    fix

--- lib/src/extensions/canvas_ext.dart
+++ lib/src/extensions/canvas_ext.dart
@@ -3,19 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 extension ExtendedCanvas on Canvas {
-  static const petals = 14, petalWeightDivisor = 2.0;
+  static const petals = 14;
+  static const xPetalWeightDivisor = 2.0;
+  static const yPetalWeightDivisor = 2.0;
+  static const radiusDivisor = 2.2;
 
   void drawPetals(Color color, Color highlightColor, double radius) {
     final petalShader = RadialGradient(
-      colors: [
-        highlightColor.withOpacity(.5),
-        color.withOpacity(.5),
-      ],
-      stops: const [
-        0,
-        .3,
-      ],
-    ).createShader(Rect.fromCircle(center: Offset.zero, radius: radius)),
+          colors: [
+            highlightColor.withOpacity(.5),
+            color.withOpacity(.5),
+          ],
+          stops: const [
+            0,
+            .3,
+          ],
+        ).createShader(Rect.fromCircle(center: Offset.zero, radius: radius)),
         paint = Paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = radius / 107
@@ -25,30 +28,32 @@ extension ExtendedCanvas on Canvas {
       save();
 
       rotate(2 * pi / petals * i);
-      _drawPetal(paint, radius / 3.2);
+      _drawPetal(paint, radius / radiusDivisor);
 
       restore();
     }
   }
 
   void _drawPetal(Paint paint, double radius) {
-    final path = Path()
-      ..moveTo(0, 0)
-      // Could use conicTo instead and pass a weight there, but
-      // it works better for me doing it this way.
-      ..quadraticBezierTo(
-        -radius / petalWeightDivisor,
-        radius / 2,
-        0,
-        radius,
-      )
-      ..quadraticBezierTo(
-        radius / petalWeightDivisor,
-        radius / 2,
-        0,
-        0,
-      )
-      ..close();
+    final path = Path();
+
+    path.moveTo(0, 0);
+
+    path.quadraticBezierTo(
+      -radius / xPetalWeightDivisor,
+      radius / yPetalWeightDivisor,
+      0,
+      radius,
+    );
+
+    path.quadraticBezierTo(
+      radius / xPetalWeightDivisor,
+      radius / yPetalWeightDivisor,
+      0,
+      0,
+    );
+
+    path.close();
 
     drawShadow(path, Colors.black, 1.0, false);
     drawPath(path, paint);

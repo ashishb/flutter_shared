13b550fd9d07fe2f99192cc699219ef41acbb495
Steve Gehrman <sgehrman@gmail.com>
2021-01-03 04:03:17 -0800

    fix

--- lib/src/widgets/json_viewer_screen.dart
+++ lib/src/widgets/json_viewer_screen.dart
@@ -5,15 +5,30 @@ class JsonViewerScreen extends StatelessWidget {
   const JsonViewerScreen({
     @required this.map,
     @required this.title,
+    this.onDelete,
   });
 
   final Map<String, dynamic> map;
   final String title;
+  final VoidCallback onDelete;
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> actions = [];
+    if (onDelete != null) {
+      actions = [
+        IconButton(
+          iconSize: 18,
+          icon: const Icon(Icons.delete),
+          onPressed: onDelete,
+        ),
+      ];
+    }
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(
+        title: Text(title),
+        actions: actions,
+      ),
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: JsonViewerWidget(map),

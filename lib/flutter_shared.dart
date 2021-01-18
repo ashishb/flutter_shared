3d456c0e0b8a757232e4c46243fa91e11b0faca1
Steve Gehrman <sgehrman@gmail.com>
2021-01-17 20:24:25 -0800

    fix

--- lib/flutter_shared.dart
+++ lib/flutter_shared.dart
@@ -1,6 +1,10 @@
 library flutter_shared;
 
 export 'flutter_shared_web.dart';
+export 'src/file_system/file_system.dart';
+export 'src/file_system/server_files.dart';
+export 'src/file_system/standard_directories.dart';
+export 'src/file_system/zip_archive.dart';
 export 'src/rive/loading.dart';
 export 'src/rive/treasure.dart';
 export 'src/themes/editor/theme_color_editor_screen.dart';

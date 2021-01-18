7026e2d9cfe5f4eaec606cfc7f704fca14e03045
Steve Gehrman <sgehrman@gmail.com>
2021-01-17 20:31:04 -0800

    fix

--- lib/flutter_shared.dart
+++ lib/flutter_shared.dart
@@ -1,7 +1,9 @@
 library flutter_shared;
 
 export 'flutter_shared_web.dart';
+export 'src/file_system/directory_listing_spec.dart';
 export 'src/file_system/file_system.dart';
+export 'src/file_system/server_file.dart';
 export 'src/file_system/server_files.dart';
 export 'src/file_system/standard_directories.dart';
 export 'src/file_system/zip_archive.dart';
@@ -11,6 +13,5 @@ export 'src/themes/editor/theme_color_editor_screen.dart';
 export 'src/themes/editor/theme_editor_screen.dart';
 export 'src/themes/editor/theme_editor_widget.dart';
 export 'src/utils/device_info_screen.dart';
-export 'src/utils/system_directories.dart';
 export 'src/widgets/colorized_text.dart';
 export 'src/widgets/gradient_text.dart';

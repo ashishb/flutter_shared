499b91bdec395b16dce6ac566c29513e43a065ce
Steve Gehrman <sgehrman@gmail.com>
2021-03-17 19:32:06 -0700

    fix

--- lib/src/utils/string_utils.dart
+++ lib/src/utils/string_utils.dart
@@ -21,7 +21,7 @@ class StrUtils {
     return prettyprint;
   }
 
-  static String toPrettyList(List list, {bool print = false}) {
+  static String toPrettyList(List<Map> list, {bool print = false}) {
     const JsonEncoder encoder = JsonEncoder.withIndent('  ');
     final String prettyprint = encoder.convert(list);
 

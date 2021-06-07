3c153a960f3fac04692412beeed9d4ad543c8449
Steve Gehrman <sgehrman@gmail.com>
2021-06-07 15:57:02 -0700

    fix

--- lib/flutter_shared.dart
+++ lib/flutter_shared.dart
@@ -8,6 +8,7 @@ export 'src/file_system/zip_archive.dart';
 export 'src/themes/editor/theme_color_editor_screen.dart';
 export 'src/themes/editor/theme_editor_screen.dart';
 export 'src/themes/editor/theme_editor_widget.dart';
+export 'src/utils/create_icon_screen.dart';
 export 'src/utils/device_info_screen.dart';
 export 'src/widgets/image/image_viewer.dart';
 export 'src/widgets/image/super_image.dart';
